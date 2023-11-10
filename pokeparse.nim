47c52ae418550efee72834d04cf8aeee1aedb5b3
Laaen@hotmail.fr <Laaen@hotmail.fr>
2023-11-10 17:37:47 +0100

    Bug fixes + Pokemon type

--- pokeparse.nim
+++ pokeparse.nim
@@ -10,6 +10,12 @@ import std/enumutils
 import std/strformat
 import std/parseutils
 import std/random
+import std/unicode
+
+# Couleurs pour les types de pokemons
+type
+  TypeColor = enum
+    Flying = ("blue")
 
 type
   LogType = enum
@@ -23,16 +29,23 @@ type
   Pokemon* = object of RootObj
     nom : string
     tier : string
+    types : seq[string]
 
 proc tier* (p : Pokemon) : string = p.tier
 proc nom* (p : Pokemon) : string = p.nom
+proc types* (p: Pokemon) : seq[string] = p.types
+proc repr (p: Pokemon) : string = 
+  &"{p.nom.toLower.capitalize} | Type(s) : {p.types.join(sep = \" \")}"
+
+
+proc getV(h : JsonNode, k : string) : string =
+  return h[k].getSTr().toUpper()
 
 proc log(l_type : LogType, contenu : string) = 
   ## Permet de logger des infos ou erreurs
   case l_type
     of LogType.Error: stdout.styledWriteLine(fgRed, contenu & "\n")
     of LogType.Info : stdout.styledWriteLine(fgYellow, contenu & "\n")
-    else : echo contenu
 
 proc getPkdx() : auto = 
   ## Retreives the json, parses it and returns a seq of Pokemon objects 
@@ -47,20 +60,15 @@ proc getPkdx() : auto =
     client.close()
   let parsed_content = parseJson(contenu)
 
-  # On crée la liste de pokémons
+  # On crée la liste de pokémons, on exclut les pokémons sans tier
   result = collect:
     for e in parsed_content.keys: 
       if parsed_content[e].hasKey("tier"):
-        Pokemon(nom : parsed_content[e]["name"].getStr(), tier : parsed_content[e]["tier"].getStr())
-
-
-proc getByTier(liste_pk : openArray[Pokemon], tier: string): auto =
-  ## On récupère tous les pokémons du tier choisi
-  # On crée un openarray qui ne contient que les poké du tier choisi
-  result = collect:
-    for p in liste_pk:
-      if p.tier == tier:
-        p 
+        # On récupère la liste de types
+        let types = collect:
+          for elt in items(parsed_content[e]["types"]):
+            elt.getStr()
+        Pokemon(nom : parsed_content[e].getV("name"), tier : parsed_content[e].getV("tier"), types : types)
 
 proc recupRand(liste_pk : openArray[Pokemon], nombre : int): auto = 
   ## Retourne un openAr ray de nombre pokémons choisis au hasard dans la liste liste_pk
@@ -94,7 +102,7 @@ var nombre = 0 # Nombre de pokémons à tirer
 while not (entree =~ re"[q|Q]"):
   
   stdout.write prompt_tier
-  entree = readLine(stdin)
+  entree = readLine(stdin).toUpper()
   
   # On essaie de récupérer la liste des pokémons du tier choisi, si len liste == 0, on reboucle
   let liste_pkmn = pokedex.filter((p) => p.tier == entree)
@@ -110,9 +118,11 @@ while not (entree =~ re"[q|Q]"):
     let nb = readline(stdin)
     discard parseint(nb, nombre)
   
+  echo ""
   # On a le bon nombre, on va récupérer X pokémons du tier demande, et les afficher
   for e in liste_pkmn.recupRand(nombre):
-    echo &"{e.nom}"
+    echo e.repr
+  echo ""
 
   # On reset le nombre et l'entrée
   nombre = 0
